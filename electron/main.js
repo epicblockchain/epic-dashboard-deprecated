05361f8bcc3feaff1e10812a54a2ee2a2fecc4eb
Dan Nicolau <dnicolau@epic001.epic>
2021-01-25 15:38:25 -0500

    fixed a crash where an array of null was returned from the miner

--- electron/main.js
+++ electron/main.js
@@ -291,15 +291,20 @@ function calculateAverages(miner){
             console.log(e);
         }
     }
-    if (miner.history.status === 'completed'){
-        const historyClone = miner.history.data.History.map(el => {
-            return el.Hashrate;
-        });
-        //24 elements from the back in reverse, a better way might be to use the timestamp on the history object
-        const reverseHistory = historyClone.reverse().slice(0, 24);
-        // console.log(miner.ip)
-        // console.log(miner.history.data.History);
-        // console.log(reverseHistory);
+    if (miner.history.status === 'completed' && miner.history.length > 0){
+        try {
+            const historyClone = miner.history.data.History.map(el => {
+                return el.Hashrate;
+            });
+            //24 elements from the back in reverse, a better way might be to use the timestamp on the history object
+            const reverseHistory = historyClone.reverse().slice(0, 24);
+            // console.log(miner.ip)
+            // console.log(miner.history.data.History);
+            // console.log(reverseHistory);
+        } catch (e) {
+            console.log(e);
+            return averages;
+        }
         if (reverseHistory.length >= 1){
             try {
                 averages["1hr"].hashrate = reverseHistory[0];
