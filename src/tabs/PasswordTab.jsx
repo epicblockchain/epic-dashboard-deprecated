ce83358d94a1e3cb658f10866373d15be5ecc17c
Dan Nicolau <dnicolau@epicblockchain.io>
2021-06-10 20:19:12 +0000

    Update .gitlab-ci.yml

---
+++ src/tabs/PasswordTab.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Button, TextField } from '@material-ui/core';
+
+export class PasswordTab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {pass1: '', pass2: '', error: false, password: ''};
+
+        this.updatePass1 = this.updatePass1.bind(this);
+        this.checkMatch = this.checkMatch.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+    }
+    
+    updatePass1(e) {
+        this.setState({pass1: e.target.value});
+    }
+
+    checkMatch(e) {
+        if (this.state.pass1 == e.target.value) {
+            this.setState({error: false, pass2: e.target.value});
+        } else {
+            this.setState({error: true, pass2: e.target.value});
+        }
+    }
+
+    updatePassword(e) {
+        this.setState({password: e.target.value});
+    }
+
+    render() {
+        return(
+            <div style={{padding: '12px 0'}}>
+                <TextField variant="outlined" label="New Password" type="password" onChange={this.updatePass1}
+                    value={this.state.pass1} margin="dense"
+                />
+                <TextField variant="outlined" label="Confirm New Password" type="password" onChange={this.checkMatch}
+                    value={this.state.pass2} error={this.state.error} margin="dense"
+                    helperText={this.state.error ? 'Passwords do not match' : ''}
+                />
+                <TextField variant="outlined" label="Password" type="password" onChange={this.updatePassword} margin="dense"/>
+                <Button onClick={() => {
+                        this.props.handleApi('/password', this.state, this.props.selected);
+                    }} variant="contained" color="primary"
+                    disabled={!this.state.pass1 || !this.state.pass2 || !this.state.password || !this.props.selected.length}
+                >
+                    Apply
+                </Button>
+            </div>
+        );
+    }
+}
